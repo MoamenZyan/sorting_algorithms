cb14ca8404768ce2599749b749d78f7165b65931
MoamenZyan <moamenzyan20@gmail.com>
2024-01-21 01:29:54 +0200

    bubble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - bubble sort function
+ * @array: the passed array
+ * @size: size of the array
+ * Return: Void
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	int tmp;
+
+	while (size != 0)
+	{
+	i = 0;
+
+	while (i < size)
+	{
+		if ((i + 1) < size)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+			}
+		}
+		i++;
+	}
+	size--;
+	}
+}
