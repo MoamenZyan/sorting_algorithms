c981e0c743c72e529dac258cc768def0c46b1b3d
Moamen Zyan <moamenzyan20@gmail.com>
2024-01-24 00:40:54 +0200

    2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Selection sorting function
+ * @array: passed array
+ * @size: size of the array
+ * Return: Void
+*/
+
+void selection_sort(int *array, size_t size)
+{
+	int i, j, min, tmp, index;
+
+	for (i = 0; i < size; i++)
+	{
+		min = array[i];
+		for (j = i + 1; j < size; j++)
+		{
+			if (min > array[j])
+			{
+				min = array[j];
+				index = j;
+			}
+		}
+		if (i == size - 1)
+		{
+			break;
+		}
+		tmp = array[i];
+		array[i] = min;
+		array[index] = tmp;
+		print_array(array, size);
+	}
+}
--- sort.h
+++ sort.h
@@ -8,6 +8,7 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
+
 typedef struct listint_s
 {
     const int n;
@@ -27,5 +28,6 @@ void bubble_sort(int *array, size_t size);
 
 void insertion_sort_list(listint_t **list);
 
+void selection_sort(int *array, size_t size);
 
 #endif
